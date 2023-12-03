50b4cafe044e7b47a6c4642fccf51cfba5ed5ae6
KaiqueWilliam <kaiquewilliamsilva@gmail.com>
2023-12-03 11:12:34 -0300

    update @KaiqueWilliam

--- src/Requerimentos/ValidarSolicitacao.java
+++ src/Requerimentos/ValidarSolicitacao.java
@@ -11,7 +11,8 @@ import java.util.*;
 public class ValidarSolicitacao {
     static Base base = new Base();
 
-    public void validaReserva(Date diaReserva, List<Laboratorios> statusLaboratorio, Boolean statusDisciplina, Boolean statusProfessor, String disciplinaUser, String professorUser, boolean statusAlunos) {
+    public void validaReserva(Date diaReserva, List<Laboratorios> statusLaboratorio, Boolean statusDisciplina,
+            Boolean statusProfessor, String disciplinaUser, String professorUser, boolean statusAlunos) {
         Date diaUser = new Date();
         if (diaReserva.before(diaUser))
             System.out.println(SituacaoReserva.REPROVADO);
@@ -35,8 +36,34 @@ public class ValidarSolicitacao {
             }
         }
 
+        switch (professorUser) {
+            case "Professor01", "Professor02", "Professor03", "Professor04", "Professor05", "Professor06",
+                    "Professor07", "Professor08", "Professor09", "Professor010" -> {
+                if (disciplinaUser != "BES005" || disciplinaUser != "BES006" || disciplinaUser != "BES008") {
+                    System.out.println(SituacaoReserva.REPROVADO);
+                } else {
+                    System.out.println(SituacaoReserva.APROVADO);
+                }
+            }
+            case "Professor011", "Professor012", "Professor013", "Professor014" -> {
+                if (disciplinaUser != "BES011" || disciplinaUser != "BES012" || disciplinaUser != "BES020"
+                        || disciplinaUser != "BES026") {
+                    System.out.println(SituacaoReserva.REPROVADO);
+                } else {
+                    System.out.println(SituacaoReserva.APROVADO);
+                }
+            }
+            case "Professor015" -> {
+                if (disciplinaUser != "BES038" || disciplinaUser != "BES049" || disciplinaUser != "BES048") {
+                    System.out.println(SituacaoReserva.REPROVADO);
+                } else {
+                    System.out.println(SituacaoReserva.APROVADO);
 
+                }
 
+            }
+        }
 
     }
+
 }
